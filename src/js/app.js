6145f84b262f95cc9f0339cda8c55006b27b0b49
d3moid <d3moid@users.noreply.github.com>
2015-12-11 14:39:33 +0200

    Initial Commit for Pomodoro project.

---
+++ src/js/app.js
@@ -0,0 +1,53 @@
+ // duration: seconds
+function Pomodoro(duration) {
+	this.length = duration || 25 * 60;
+	this.timer = this.length;
+	this.isRunning = false;
+	this.isPaused = true;
+}
+
+Pomodoro.prototype.clock = function() {
+	var minutes = parseInt(this.timer / 60, 10);
+	var seconds = parseInt(this.timer % 60, 10);
+	minutes = (minutes < 10) ? '0' + minutes : minutes;
+	seconds = (seconds < 10) ? '0' + seconds : seconds;
+	if(!this.isPaused) {
+		console.log(minutes, seconds);
+		if (--this.timer < 0) {
+			this.ringTimer();
+			this.resetTimer();
+		}
+	}
+};
+
+Pomodoro.prototype.startTimer = function() {
+	this.isRunning = true;
+	this.isPaused = false;
+	// if this is not set then calling the setInterval function with this.clock will set 'this' to global
+	var me = this;
+	this.interval = setInterval(function() {
+		me.clock();
+	}, 1000);
+};
+
+Pomodoro.prototype.resetTimer = function() {
+	this.isPaused = true;
+	clearInterval(this.interval);
+	this.isRunning = false;
+	this.setTimer(this.length);
+};
+
+Pomodoro.prototype.pauseTimer = function() {
+	this.isPaused = !this.isPaused;
+};
+
+Pomodoro.prototype.ringTimer = function() {
+	console.log('Ring, ring!');
+};
+
+Pomodoro.prototype.setTimer = function(duration) {
+	this.length = duration || 25 * 60;
+	this.timer = this.length;
+};
+
+var p = new Pomodoro(5);
